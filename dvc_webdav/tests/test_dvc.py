9dc945788a20b0a14c1064af176e6ee5646c676c
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-10-31 16:11:24 +0545

    test

--- dvc_webdav/tests/test_dvc.py
+++ dvc_webdav/tests/test_dvc.py
@@ -7,6 +7,19 @@ from dvc.testing.remote_tests import (  # noqa, pylint: disable=unused-import
 )
 
 
+@pytest.fixture(autouse=True)
+def mocked_odb(mocker):
+    from dvc_objects.db import ObjectDB
+
+    oinit = ObjectDB._init
+
+    def init(self, *args, **kwargs):
+        if self.fs.protocol != "webdav":
+            return oinit(self, *args, **kwargs)
+
+    yield mocker.patch.object(ObjectDB, "_init", init)
+
+
 @pytest.fixture
 def remote(make_remote):
     yield make_remote(name="upstream", typ="webdav")
