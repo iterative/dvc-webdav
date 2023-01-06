9eea0098923df28dc3c743faaa20dc09ead3b173
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2023-01-06 17:22:06 +0545

    fix linting

--- dvc_webdav/tests/conftest.py
+++ dvc_webdav/tests/conftest.py
@@ -1,3 +1,3 @@
-from dvc.testing.fixtures import *  # noqa, pylint: disable=wildcard-import,unused-import
+from dvc.testing.fixtures import *  # noqa, pylint: disable=unused-wildcard-import,wildcard-import
 
-from .fixtures import *  # noqa, pylint: disable=wildcard-import,unused-import
+from .fixtures import *  # noqa, pylint: disable=unused-wildcard-import,wildcard-import
